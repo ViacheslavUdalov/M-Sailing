5ed9e8b76d67f7d037f430616d411ac5ebc38144
Vyacheslav Udalov <slava187115@gmail.com>
2024-10-27 14:43:31 +0300

    удалён vscode 2

--- Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b02d1a67a1802437addf8401d66dbe70248e563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881698f120d4adaefcebb0bbbc106399bb2b92d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
